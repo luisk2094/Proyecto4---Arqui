91679e2f9dd5cd9d1f8711ee7900dc1507eb2846
luisk2094 <luisk2094@gmail.com>
2018-11-16 21:38:37 -0600

    version 2, php funcional

--- HTML - CSS - JS/php/insertarLibro.php
+++ HTML - CSS - JS/php/insertarLibro.php
@@ -1,10 +1,4 @@
 <?php
-echo "declarando variables";
-$host = ¨localhost¨;
-$username = "root";
-$password = ¨¨;
-$db = ¨arquitecturainformacion¨;
-
 echo "antes if";
 if (isset($_POST['titulo'])
  and !empty($_POST['titulo']) and
@@ -17,18 +11,18 @@ if (isset($_POST['titulo'])
  isset($_POST['imagen'])
   and !empty($_POST['imagen'])) { echo "estoy en el if";
 
-$con=mysqli_connect($host, $username, $password)or die("Problemas al conectar");
-mysqli_select_db($con, $db)or die("Problemas al conectar la bd");
+$con=mysqli_connect("localhost", "root", "")or die("Problemas al conectar");
+mysqli_select_db($con, "arquitecturainformacion")or die("Problemas al conectar la bd");
 
-echo "INSERT INTO 'Libro' ('Leido', 'Favorito', 'Titulo', 'Autor', 'Idioma', 'Descripcion', 'Imagen')
- VALUES (false, false, '$_POST[titulo]', '$_POST[autor]', '$_POST[idioma]', '$_POST[resumen]', '$_POST[imagen]')";
+echo "INSERT INTO `Libro` (`Leido`, `Favorito`, `Titulo`, `Autor`, `Idioma`, `Descripcion`, `Imagen`)
+ VALUES (false, false, `$_POST[titulo]`, `$_POST[autor]`, `$_POST[idioma]`, `$_POST[resumen]`, `$_POST[imagen]`)";
 
-mysqli_query($con, "INSERT INTO 'Libro' ('Leido', 'Favorito', 'Titulo', 'Autor', 'Idioma', 'Descripcion', 'Imagen')
+mysqli_query($con,  "INSERT INTO `Libro` (`Leido`, `Favorito`, `Titulo`, `Autor`, `Idioma`, `Descripcion`, `Imagen`)
  VALUES (false, false, '$_POST[titulo]', '$_POST[autor]', '$_POST[idioma]', '$_POST[resumen]', '$_POST[imagen]')");
 echo "datos insertados";
 }else {
   echo "Problemas al insertar datos";
 }
 mysqli_close($con);
-//header('Location: detalles.html');
+header('Location: ../index.html');
  ?>
