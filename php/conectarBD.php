951e81498453045ce49151e48636b191b73026d2
luisk2094 <luisk2094@gmail.com>
2018-11-18 13:17:32 -0600

    Version Final Final DB

--- php/conectarBD.php
+++ php/conectarBD.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost", "root", "")or die("Problemas al conectar");
-mysqli_select_db($con, "arquitecturainformacion")or die("Problemas al conectar la bd");
+$con=mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "b3d8a73364892f", "b8569118")or die("Problemas al conectar");
+mysqli_select_db($con, "heroku_e196db407567371")or die("Problemas al conectar la bd");
 
 ?>
