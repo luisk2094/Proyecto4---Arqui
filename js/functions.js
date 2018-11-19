80150f083b2bb0764629c2741a159849ee72414a
luisk2094 <luisk2094@gmail.com>
2018-11-18 18:40:31 -0600

    prueba 2 filter

--- js/functions.js
+++ js/functions.js
@@ -2,15 +2,15 @@
 $( document ).ready(function() {
     $('#menu_on').click(function(){
     	$('body').toggleClass('visible_menu');
-    })
+    });
+
+    $('#tablasPHP').DataTable( {
+        paging: true,
+        searching: true,
+    });
 });
 
 // CONVERTIR LAS FILAS EN LINKS
 function CrearEnlace(url) {
     location.href=url;
 }
-
-$('#tablasPHP').DataTable( {
-    paging: true,
-    searching: true,
-} );
